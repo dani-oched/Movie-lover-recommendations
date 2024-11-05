28ab89200493f557cdc036d1fcb6d40a2828dd7b
dani-oched <daniel.oje.che@gmail.com>
2024-11-05 19:21:57 +0100

    Slightly improved look

--- User_recs/app.py
+++ User_recs/app.py
@@ -9,16 +9,18 @@ def index():
 
 @app.route('/compare', methods=['POST'])
 def compare():
-    usernames = request.form.getlist('usernames')
-    usernames = usernames[0].split(', ')
+    # usernames = request.form.getlist('usernames')
+    # usernames = usernames[0].split(', ')
+    user1 = request.form.getlist('user1')[0]
+    user2 = request.form.getlist('user2')[0]
 
-    common_movies = getAllCommonFilms(*usernames)
+    common_movies = getAllCommonFilms(user1, user2)
     img_urls = getAllPosters(common_movies)
     movies = []
     for i, j in zip(common_movies, img_urls):
         movies.append({'name': i, 'image_url': j, 'link': 'https://letterboxd.com/film/'+i+'/'})
     
-    return render_template('index.html', common_movies=movies, usernames=usernames)
+    return render_template('index.html', common_movies=movies, usernames=[user1, user2])
 
 if __name__ == '__main__':
     app.run(debug=True)
